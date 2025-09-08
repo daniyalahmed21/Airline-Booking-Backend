f304ad9bd77e4d258b6a225ef6d1e05449a6191b
Daniyal Ahmed <da9033431@gmail.com>
2025-09-08 20:22:54 +0100

    added airplane controller layer

--- src/controllers/airplaneController.js
+++ src/controllers/airplaneController.js
@@ -1,1 +1,26 @@
+import AirplaneService from "../services/airplaneService";
 
+const { StatusCodes } = require("http-status-codes");
+
+export async function createAirplane(req, res) {
+  try {
+    const airplane = await AirplaneService.createAirplane({
+      modelNumber: req.body.modelNumber,
+      capacity: req.body.capacity,
+    });
+
+    res.status(StatusCodes.CREATED).json({
+      status: "success",
+      data: airplane,
+      message: "Successfully created an airplane",
+      error: {},
+    });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: "false",
+      data: {},
+      message: "Something went wrong while creating an airplane",
+      error: error,
+    });
+  }
+}
