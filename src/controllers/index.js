aed42cb44fe84d1c5ccb0d71e36c2c2874123f50
Daniyal Ahmed <da9033431@gmail.com>
2025-09-12 21:34:03 +0100

    Added Airport routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,11 @@
 import * as AirplaneController from "./airplaneController.js";
 import * as CityController from "./cityController.js";
+import * as AirportController from "./airportController.js";
+
+
 
 export  {
   AirplaneController,
-  CityController
+  CityController,
+  AirportController
 };
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,12 @@
 import Express from "express";
 import airplaneRouter from "./airplaneRoutes.js";
 import cityRouter from "./cityRoutes.js";
+import airportRouter from "./airportRoutes.js";
 
 const v1Router = Express.Router();
 
 v1Router.use("/airplanes", airplaneRouter);
 v1Router.use("/cities", cityRouter);
+v1Router.use("/airports", airportRouter);
 
 export default v1Router;
